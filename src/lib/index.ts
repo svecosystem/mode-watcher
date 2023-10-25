e0ce5ac81e28f2d52bab050dd5bc1281575ccfc7
Olle Månsson <31876997+ollema@users.noreply.github.com>
2023-10-25 15:40:05 +0000

    export getModeOsPrefers

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,3 @@
-import { setMode, toggleMode, mode } from './mode.js';
+import { setMode, toggleMode, mode, getModeOsPrefers } from './mode.js';
 export { default as ModeWatcher } from './mode-watcher.svelte';
-export { setMode, toggleMode, mode };
+export { setMode, toggleMode, mode, getModeOsPrefers };
