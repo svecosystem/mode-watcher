77361eaf03476aae23be5b55ee8ecdd1b2010a42
Olle Månsson <31876997+ollema@users.noreply.github.com>
2023-10-28 20:15:07 +0200

    track changes

--- src/lib/mode-watcher.svelte
+++ src/lib/mode-watcher.svelte
@@ -1,11 +1,37 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { getSystemPrefersMode, setInitialClassState, setActiveMode } from './mode.js';
+	import {
+		getSystemPrefersMode,
+		getUserPrefersMode,
+		setActiveMode,
+		setInitialClassState
+	} from './mode';
+
+	// track `prefers-color-scheme` if no user preference is set
+	export let track = true;
 
 	onMount(() => {
 		if (!('mode' in localStorage)) {
 			setActiveMode(getSystemPrefersMode());
 		}
+
+		if (!track) {
+			return;
+		}
+
+		const mql = window.matchMedia('(prefers-color-scheme: light)');
+
+		const listener = () => {
+			if (getUserPrefersMode() === 'system') {
+				setActiveMode(getSystemPrefersMode());
+			}
+		};
+
+		mql.addEventListener('change', listener);
+
+		return () => {
+			mql.removeEventListener('change', listener);
+		};
 	});
 </script>
 
