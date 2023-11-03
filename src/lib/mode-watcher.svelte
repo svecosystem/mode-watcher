c9bf805a0cf4b2ea173a123af197ac716498ad92
HubbeDev <privat@responsivhub.com>
2023-11-03 22:19:53 +0100

    Fix bug missing withoutTransition in head

--- src/lib/mode-watcher.svelte
+++ src/lib/mode-watcher.svelte
@@ -6,6 +6,7 @@
 		setActiveMode,
 		setInitialClassState
 	} from './mode';
+	import { browser } from '$app/environment';
 
 	// track `prefers-color-scheme` if no user preference is set
 	export let track = true;
@@ -33,10 +34,8 @@
 			mql.removeEventListener('change', listener);
 		};
 	});
-</script>
 
-<svelte:head>
-	<!-- This causes the new eslint-plugin-svelte: https://github.com/sveltejs/eslint-plugin-svelte/issues/492 -->
-	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
-	{@html `<\u{73}cript nonce="%sveltekit.nonce%">(${setInitialClassState.toString()})();</script>`}
-</svelte:head>
+	if (browser) {
+		setInitialClassState();
+	}
+</script>
