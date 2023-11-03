7c5c3de971a52d922e5914a72706df987f52102c
HubbeDev <privat@responsivhub.com>
2023-11-03 22:41:23 +0100

    Readded svelthead and removed withoutTransition in setInitialClassState

--- src/lib/mode-watcher.svelte
+++ src/lib/mode-watcher.svelte
@@ -6,7 +6,6 @@
 		setActiveMode,
 		setInitialClassState
 	} from './mode';
-	import { browser } from '$app/environment';
 
 	// track `prefers-color-scheme` if no user preference is set
 	export let track = true;
@@ -34,8 +33,10 @@
 			mql.removeEventListener('change', listener);
 		};
 	});
-
-	if (browser) {
-		setInitialClassState();
-	}
 </script>
+
+<svelte:head>
+	<!-- This causes the new eslint-plugin-svelte: https://github.com/sveltejs/eslint-plugin-svelte/issues/492 -->
+	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+	{@html `<\u{73}cript nonce="%sveltekit.nonce%">(${setInitialClassState.toString()})();</script>`}
+</svelte:head>
